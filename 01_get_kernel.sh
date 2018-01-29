f801387c50bcda278e46848698b1e507fd8b0f20
zac87 <35594146+zac87@users.noreply.github.com>
2018-01-29 12:38:15 +0100

    switch to stable kernel

--- 01_get_kernel.sh
+++ 01_get_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-KERNEL_SOURCE_URL=https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.15.tar.xz
+KERNEL_SOURCE_URL=https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.15.tar.xz
 
 
 # Grab everything after the '=' character.
